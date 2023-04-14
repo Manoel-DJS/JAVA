feeb2c7f6901276e650a50305c4b9e18ad00fd06
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-04-14 17:30:33 -0300

    Update App.java

--- Prática 2/Quest01/src/App.java
+++ Prática 2/Quest01/src/App.java
@@ -1,9 +1,18 @@
-
+// para Testes
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("OLa mundo!");
+
+        Scanner ler = new Scanner(System.in);
+    System.out.println("Anos de vida: ");
+    int anos = ler.nextInt();
+    System.out.println("Meses de vida: ");
+    int meses = ler.nextInt();
+    System.out.println("dias de vida: ");
+    int dias = ler.nextInt();
+    int resultado = anos*365 + meses*30 + dias;
+    System.out.println("Total de dias: " + resultado);
+    }
     }
 
-    
-
-}
