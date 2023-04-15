075655cb7215196ed7323dc37cb85cfc623e3f1c
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-04-15 09:56:07 -0300

    Quest01 feita
    Muita bagunça

--- Lista/Quest01.java
+++ Lista/Quest01.java
@@ -1,5 +1,22 @@
 package Lista;
+import java.util.Scanner;
 
 public class Quest01 {
-    
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        // Entrada de Dados
+        System.out.println("Digite um número(A): ");
+        int numA = ler.nextInt();
+
+        System.out.println("Digite um número(B): ");
+        int numB = ler.nextInt();
+        
+        // Soluçando
+        if(numA > numB){
+            System.out.println("Valor A: " + numA);
+        } else{
+            System.out.println("Valor B: " + numB);
+        }
+    }
 }
---
+++ Prática 2/Quest01/src/teste.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class teste {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        // Entrada de Dados
+        System.out.println("Digite um número(A): ");
+        int numA = ler.nextInt();
+
+        System.out.println(numA);
+    }
+}
