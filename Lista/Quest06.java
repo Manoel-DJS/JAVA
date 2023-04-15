cfed74a5a57899265a45985be2b503cfae1704a4
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-04-15 12:46:50 -0300

    Finalizando Lista, falta 1

--- Lista/Quest06.java
+++ Lista/Quest06.java
@@ -1,7 +1,42 @@
+/** 
+
+Faça um programa calcula o total de uma hospedagem em um hotel. Ele cobra R$ 60.00 a diária e mais uma taxa de serviço:
+R$ 5.50 por diária, se o número de diárias for maior que 15;
+R$ 6.00 por diária, se o número de diárias for igual a 15;
+R$ 8.00 por diária, se o número de diárias for menor que 15.
+
+**/
+
 package Lista;
 import java.util.Scanner;
 public class Quest06 {
     public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
         
+        int diaria = 60; // valor fixo
+        float taxa = 0;
+
+        // entrada de dados
+        System.out.println("Digite quantos dias você ficará em nosso hotel: ");
+        int dias = ler.nextInt();
+
+        // condições
+        if(dias > 15){
+            taxa = (float) 5.50;
+            System.out.println("taxa de R$5.50");
+            System.out.println("Valor da Hospedagem: R$" + ((diaria * dias) + (taxa * dias)) );
+        } else if(dias == 15){
+            taxa = (float) 6;
+            System.out.println("taxa de R$6.00");
+            System.out.println("Valor da Hospedagem: R$" + ((diaria * dias) + (taxa * dias)) );
+        } else{
+            taxa = (float) 8;
+            System.out.println("taxa de R$8.00");
+            System.out.println("Valor da Hospedagem: R$" + ((diaria * dias) + (taxa * dias)) );
+        }
+
+
+
+
     }
 }
---
+++ Lista/Quest07.java
@@ -0,0 +1,79 @@
+/**
+
+Crie um programa que lê um número entre 1 e 12, correspondendo
+a um dos meses do ano onde o mês 1 é Janeiro e 12 é dezembro.
+Ao final, o programa deve imprimir uma mensagem identificando,
+com base no número digitado, o nome do mês e a estação. 
+
+Exemplo para o mês 1: "Mês: Janeiro – Estação: Verão"
+ 
+**/
+
+package Lista;
+import java.util.Scanner;
+public class Quest07 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        // entrada de dados
+        System.out.println("Digite um número de 1 a 12: ");
+        int numero = ler.nextInt();
+        String mes = "";
+        switch(numero){
+            case 1: 
+            mes = "Mês: Janeiro // Estação: Verão";
+            break;
+
+            case 2: 
+            mes = "Mês: Fevereiro // Estação: Verão";
+            break;
+            
+            case 3: 
+            mes = "Mês: Março // Estação: Outono";
+            break;
+            
+            case 4: 
+            mes = "Mês: Abril // Estação: Outono";
+            break;
+            
+            case 5: 
+            mes = "Mês: Maio // Estação: Outono";
+            break;
+            
+            case 6: 
+            mes = "Mês: Junho // Estação: Inverno";
+            break;
+            
+            case 7: 
+            mes = "Mês: Julho // Estação: Inverno";
+            break;
+            
+            case 8: 
+            mes = "Mês: Agosto // Estação: Inverno";
+            break;
+            
+            case 9: 
+            mes = "Mês: Setembro // Estação: Primavera";
+            break;
+            
+            case 10: 
+            mes = "Mês: Outubro // Estação: Primavera";
+            break;
+            
+            case 11: 
+            mes = "Mês: Novembro // Estação: Primavera";
+            break;
+            
+            case 12: 
+            mes = "Mês: Dezembro // Estação: Verão";
+            break;
+            
+            default:
+            mes = "Entrada inválida || Número Inválido";
+            break;
+        }
+        System.out.println("O número: " + numero + " Corresponde ao:");
+        System.out.println(mes);
+
+    }
+}
