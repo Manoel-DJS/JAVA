b9f68d43102899a2540789d46ee5165b2fd3efea
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2023-04-15 15:22:58 -0300

    Lista Prática 2 finalizada

--- Lista/Quest08.java
+++ Lista/Quest08.java
@@ -10,14 +10,6 @@ Venda a Prazo 90 dias - acréscimo de 5%
 Venda com cartão de débito - desconto de 8%
 Venda com cartão de crédito - desconto de 7%
 
-        System.out.println("____________________________________________________________");
-        System.out.println("A vista, digite AVISTA (10% de desconto): ");
-        System.out.println("Venda a Prazo 30 dias, digite PRAZO30 (5% de desconto): ");
-        System.out.println("Venda a Prazo 60 dias, digite PRAZO60 (Mesmo preço): ");
-        System.out.println("Venda a Prazo 90 dias, digite PRAZO90 (5% a mais): ");
-        System.out.println("Venda com cartão de débito, digite CARTDEB (8% de desconto): ");
-        System.out.println("Venda com cartão de crédito, digite CARTCRE (7% de desconto): ");
-        System.out.println("____________________________________________________________");
 
 
 **/
@@ -27,17 +19,59 @@ import java.util.Scanner;
 public class Quest08 {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        float desconto = 0;
+
         // Entrada de dados
         System.out.println("Valor do produto: ");
         float valor = ler.nextFloat();
   
+        System.out.println("____________________________________________________________");
+        System.out.println("A vista, digite (1) (10% de desconto): ");
+        System.out.println("Venda a Prazo 30 dias, digite (2) (5% de desconto): ");
+        System.out.println("Venda a Prazo 60 dias, digite (3) (Mesmo preço): ");
+        System.out.println("Venda a Prazo 90 dias, digite (4) (5% a mais): ");
+        System.out.println("Venda com cartão de débito, digite (5) (8% de desconto): ");
+        System.out.println("Venda com cartão de crédito, digite (6) (7% de desconto): ");
+        System.out.println("____________________________________________________________");
 
-        System.out.println("Valor do produto: ");
-        String valors = ler.nextLine();
+        System.out.println("Escolha a forma de pagamento conforme a tabela acima: ");
+        int forma = ler.nextInt();
 
+        double valortotal = 0;
 
+        switch(forma){
+            case 1:
+            valortotal = valor * 0.9;
+            System.out.println("Valor da Venda: " + valortotal);
+            break;
+            
+            case 2:
+            valortotal = valor * 0.95;
+            System.out.println("Valor da Venda: " + valortotal);
+            break;
+            
+            case 3:
+            valortotal = valor;
+            System.out.println("Valor da Venda: " + valortotal);
+            break;
+            
+            case 4:
+            valortotal = valor * 1.05;
+            System.out.println("Valor da Venda: " + valortotal);
+            break;
 
+            case 5:
+            valortotal = valor * 0.92;
+            System.out.println("Valor da Venda: " + valortotal);
+            break;
 
+            case 6:
+            valortotal = valor * 0.93;
+            System.out.println("Valor da Venda: " + valortotal);
+            break;
+            
+            default:
+            System.out.println("Forma de pagamento inválida // Digite um número de 1 a 6");
+            break;
+        }
     }
 }
