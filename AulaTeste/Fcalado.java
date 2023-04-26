04560f956333bbea304c479a3408937785e0c256
Manoel-DJS <vinesouza9x8@gmail.com>
2023-04-25 22:04:16 -0300

    Update Fcalado.java

--- AulaTeste/Fcalado.java
+++ AulaTeste/Fcalado.java
@@ -24,7 +24,41 @@ public class Fcalado {
 
 }
 
+/** 
 
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ConversaoDeTemperatura {
+    public static void main(String[] args) {
+        // Definindo a configuração regional
+        Locale.setDefault(Locale.US);
+
+        Scanner keyboard = new Scanner(System.in);
+        double grausFahrenheit = lerTemperaturaEmGrausFahrenheit(keyboard);
+
+        keyboard.close();
+
+        double grausCelsius = converterTemperatura(grausFahrenheit);
+
+        System.out.printf("A temperatura convertida para graus Celsius é %.2f graus", grausCelsius);
+
+        
+    }
+
+    private static double lerTemperaturaEmGrausFahrenheit(Scanner keyboard) {
+        System.out.print("Informe a temperatura em graus Fahrenheit: ");
+        double grausFahrenheit = keyboard.nextDouble();
+        return grausFahrenheit;
+    }
+
+    private static double converterTemperatura(double grausFahrenheit) {
+        double grausCelsius = 5 * ((grausFahrenheit - 32) / 9);
+        return grausCelsius;
+    }
+}
+ 
+**/
 // static void não precisa de return// NAU USA
 
 // static double ou int ou float
