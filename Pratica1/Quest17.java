4e687b101fc437941c3d4fb8f18fd42b7088f4d1
Manoel-DJS <vinesouza9x8@gmail.com>
2023-05-05 11:19:48 -0300

    Update Quest17.java

--- Pratica1/Quest17.java
+++ Pratica1/Quest17.java
@@ -1,6 +1,7 @@
 /** 
 
-Faça um Programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 6 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00 ou em galões de 3,6 litros, que custam R$ 25,00.
+Faça um Programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. 
+Considere que a cobertura da tinta é de 1 litro para cada 6 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00 ou em galões de 3,6 litros, que custam R$ 25,00.
 Informe ao usuário as quantidades de tinta a serem compradas e os respectivos preços em 3 situações:
 comprar apenas latas de 18 litros;
 comprar apenas galões de 3,6 litros;
@@ -15,23 +16,47 @@ public class Quest17 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Qual o Tamanho da área a ser pintada? Digite em metros(^2)");
-        double metrosQuadrados = scanner.nextDouble();
+        // Entrada de dados
+        System.out.println("Digite o tamanho da área que será pintada: ");
+        double areaPintada = scanner.nextDouble();
 
         scanner.close();
-        double litros = metrosQuadrados / 3;
-        System.out.println("Litros de tinta: " + litros);
 
-        System.out.println("Litros: " + litros);
+        double litros = areaPintada / 6;
 
         double lata = 0;
-        for (int c = 0; c <= litros; c +=18){
+        for (int c = 0; c < litros; c +=18){
             lata += 1;
         }
-        double preçoTotal = lata * 80;
+        double preçoLatas = lata * 80;
+
+        double galoes = litros / 3.6;
+        if(galoes % 3.6 != 0){
+            galoes += 1;
+        }
+
+
+        double preçoGaloes = galoes*25; //
+
+        double misturaLata =  litros/18; //
+
+        double litrosRestantes = (litros - lata*18);
+        if(litrosRestantes < 0){
+            litrosRestantes += 18;
+        }
+
+        galoes = litrosRestantes / 3.6;
 
         
-        System.out.printf("Para pintar uma área de %.1f metros quadrados, é necessário comprar %.1f latas de tinta \n", metrosQuadrados, lata);
-        System.out.printf("Preço das latas: R$%.2f \n",preçoTotal);
-    }
+
+        double preçoTotal = galoes * 25 + lata * 80;
+
+        System.out.println("Litros: " + litros);
+        System.out.println("Lata: " + lata);
+        System.out.println("Galoes: " + galoes);
+        System.out.println(litrosRestantes);
+        System.out.printf("Preço total: R$%.2f", preçoTotal);
+
+        }
+
 }
